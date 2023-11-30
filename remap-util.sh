e57934996d366a68d539ea75fa5a126cb9002035
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2023-11-30 10:11:17 -0600

    Add files via upload

---
+++ remap-util.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#######################
+# Chromebook key remapping utility
+#  
+# Disclaimer: 
+# I am not responsible for any damage or bricking of your device
+# By using this script, you acknowledge this.
+#
+# Created by Lioncat6:
+# https://github.com/Lioncat6
+#
+#######################
